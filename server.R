51dbe8358c23d793e2f515c0d8a634ab87013894
Thomas Hsiao <tXiao95@gmail.com>
2020-09-22 15:01:51 -0400

    Functioning version of shiny app
    Shiny app now allows for parameters to be entered for both theta0 and logRR priors and runs a STAN model accordingly.
    Plots of prior and posterior for theta0 and vaccine efficacy are displayed. Other features include
    
    * Reset button for resetting all inputs to default values
    * Allow for variable user defined number of mixture components that maintain previously entered values without returning to defaults when number of components is adjusted.

--- server.R
+++ server.R
@@ -3,6 +3,10 @@ library(LaplacesDemon)
 library(ggplot2)
 library(purrr)
 
+# helper functions --------------------------------------------------------
+
+source("sim-shiny.R")
+
 stat_dist <- function(dist, ...) {
     ggplot2::stat_function(ggplot2::aes_(color = dist), ...)
 }
@@ -19,8 +23,12 @@ default_val <- function(x, value) {
     } 
 }
 
+# Server Side -------------------------------------------------------------
+
 shinyServer(function(input, output) {
-    
+
+# Handling arbitrary number of K-dim Gaussian mixture ---------------------
+
     lambda_names <- reactive(paste0("p", seq_len(input$n)))
     mu_names     <- reactive(paste0("mu", seq_len(input$n)))
     sigma_names  <- reactive(paste0("sigma", seq_len(input$n)))
@@ -37,11 +45,54 @@ shinyServer(function(input, output) {
         map(sigma_names(), ~ numericInput(.x, .x, value = isolate(input[[.x]]), min = 0) %||% "")
     })
     
+
+# Actual stan model run ---------------------------------------------------
+
+    dens <- eventReactive(input$do_sim, {
+        lambda <- map_dbl(lambda_names(), ~ default_val(input[[.x]], NA))
+        mu     <- map_dbl(mu_names(), ~ default_val(input[[.x]], NA))
+        sigma  <- map_dbl(sigma_names(), ~ default_val(input[[.x]], NA))
+        sim    <- vaccine_sim(rratio=input$rratio, 
+                               cases=input$cases, 
+                               vax_cases=input$vax_cases, 
+                               N=input$N, 
+                               lambda=lambda, 
+                               mu=mu, 
+                               sigma=sigma, 
+                               alpha=input$alpha, beta=input$beta, iter=1000)
+        
+        # Rhats should be close to 1
+        fit <- sim$fit
+        data <- sim$data
+        
+        samples <- extract(fit)
+        ve <- samples$ve
+        theta0 <- samples$theta0
+        
+        dens_ve <- density(ve)
+        dens_theta0 <- density(theta0)
+        list(dens_ve=dens_ve, dens_theta0=dens_theta0)
+    }, ignoreNULL = FALSE)
+
+# Reset button ------------------------------------------------------------
+
+    observeEvent(input$reset, {
+        reset("N")
+        reset("rratio")
+        reset("cases")
+        reset("vax_cases")
+        reset("alpha")
+        reset("beta")
+        reset("n")
+    })
+
+# Plot of theta0 ----------------------------------------------------------
+
     output$theta_prior <- renderPlot({
         ggplot(data.frame(x = c(0,1)), ggplot2::aes(x)) +
             stat_dist("prior", size = 1, fun = dbeta,
                       args = list(shape1 = input$alpha, shape2 = input$beta)) + 
-            #geom_line(data=data.frame(x = dens_theta0$x, y = dens_theta0$y), aes(x,y, col="posterior")) + 
+            geom_line(data=data.frame(x = dens()$dens_theta0$x, y = dens()$dens_theta0$y), aes(x,y, col="posterior")) + 
             theme_bw() + 
             geom_hline(yintercept=0) + 
             geom_vline(xintercept=0) + 
@@ -50,7 +101,9 @@ shinyServer(function(input, output) {
             xlab("theta0")
     
     }, res = 96)
-    
+
+# Plot of VE --------------------------------------------------------------
+
     output$ve_prior <- renderPlot({
         lambda <- map_dbl(lambda_names(), ~ default_val(input[[.x]], NA))
         mu <- map_dbl(mu_names(), ~ default_val(input[[.x]], NA))
@@ -58,7 +111,7 @@ shinyServer(function(input, output) {
         ggplot(data.frame(x = c(-.5,.5)), ggplot2::aes(x)) + 
             stat_dist("prior", size = 1, fun = dmix,
                       args = list(p=lambda, mu = mu, sigma = sigma)) + 
-            geom_line(data=data.frame(x = dens_ve$x, y = dens_ve$y), aes(x,y, col="posterior")) + 
+            geom_line(data=data.frame(x = dens()$dens_ve$x, y = dens()$dens_ve$y), aes(x,y, col="posterior")) + 
             theme_bw() + 
             geom_vline(xintercept=0.5, linetype= "dashed") + 
             geom_hline(yintercept=0) + 
