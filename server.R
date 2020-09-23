8ea5fcc3e282f9fb11951e5bd4dd605bf9106889
Thomas Hsiao <tXiao95@gmail.com>
2020-09-23 03:52:02 -0400

    Addressed several features from email
    Incorporated several features from the 09/22 email. Including
    
    * Option for default inputs for companies. Locks in the values unless "custom" is selected without deleting previous input.
    * Supressed posterior of placebo incidence. Only showing prior.
    * Option for default priors on logRR. Locks in value of number of mixture components unless "custom" is selected. Still not sure how to lock in all entries
    * Able to visualize prior in real time without having to run a model first

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(LaplacesDemon)
 library(ggplot2)
+library(plotly)
 library(purrr)
 
 # helper functions --------------------------------------------------------
@@ -25,27 +26,12 @@ default_val <- function(x, value) {
 
 # Server Side -------------------------------------------------------------
 
-shinyServer(function(input, output) {
-
-# Handling arbitrary number of K-dim Gaussian mixture ---------------------
+shinyServer(function(input, output, session) {
 
     lambda_names <- reactive(paste0("p", seq_len(input$n)))
     mu_names     <- reactive(paste0("mu", seq_len(input$n)))
     sigma_names  <- reactive(paste0("sigma", seq_len(input$n)))
     
-    output$lambda <- renderUI({
-        map(lambda_names(), ~ numericInput(.x, .x, value = isolate(input[[.x]]), min=0, max=1 ) %||% "")
-    })
-    
-    output$mu <- renderUI({
-        map(mu_names(), ~ numericInput(.x, .x, value = isolate(input[[.x]])) %||% "")
-    })
-    
-    output$sigma <- renderUI({
-        map(sigma_names(), ~ numericInput(.x, .x, value = isolate(input[[.x]]), min = 0) %||% "")
-    })
-    
-
 # Actual stan model run ---------------------------------------------------
 
     dens <- eventReactive(input$do_sim, {
@@ -74,19 +60,107 @@ shinyServer(function(input, output) {
         dens_ve <- density(ve)
         dens_theta0 <- density(theta0)
         list(dens_ve=dens_ve, dens_theta0=dens_theta0)
-    }, ignoreNULL = TRUE)
+    }, ignoreNULL = FALSE)
 
 
-# Output of Stan Code -----------------------------------------------------
+# Setting default interim analysis protocols by company -----------------------------------------------------
 
+    observeEvent(input$protocol, {
+        disable("N")
+        disable("rratio")
+        disable("cases")
+        disable("vax_cases")
+        if(input$protocol == "Moderna 1"){
+            updateNumericInput(session, "N", value = 1000)
+            updateNumericInput(session, "rratio", value = 0.5)
+            updateNumericInput(session, "cases", value = 26)
+            updateNumericInput(session, "vax_cases", value = 6)
+        } else if(input$protocol == "Moderna 2"){
+            updateNumericInput(session, "N", value = 2000)
+            updateNumericInput(session, "rratio", value = .67)
+            updateNumericInput(session, "cases", value = 30)
+            updateNumericInput(session, "vax_cases", value = 10)
+        } else if(input$protocol == "Pfizer 1"){
+            updateNumericInput(session, "N", value = 30000)
+            updateNumericInput(session, "rratio", value = .6)
+            updateNumericInput(session, "cases", value = 100)
+            updateNumericInput(session, "vax_cases", value = 30)
+        } else if(input$protocol == "Custom"){
+            enable("N")
+            enable("rratio")
+            enable("cases")
+            enable("vax_cases")
+            updateNumericInput(session, "N", value = NULL)
+            updateNumericInput(session, "rratio", value = NULL)
+            updateNumericInput(session, "cases", value = NULL)
+            updateNumericInput(session, "vax_cases", value = NULL)
+        }
+    }, ignoreInit = FALSE)
+    
+
+# Setting default priors by type ------------------------------------------
+
+    observeEvent(input$prior, {
+        disable("n")
+        if(input$prior == "Very Skeptical"){
+            updateNumericInput(session, "n", value = 2)
+            
+            output$lambda <- renderUI(list(numericInput("p1", "p1", value = .8, min = 0, max = 1),
+                                           numericInput("p2", "p2", value = .2, min = 0, max = 1)))
+            
+            output$mu <- renderUI(list(numericInput("mu1", "mu1", value = 0),
+                                       numericInput("mu2", "mu2", value = -.38)))
+            
+            output$sigma <- renderUI(list(numericInput("sigma1", "sigma1", value = 0.1, min=0),
+                                          numericInput("sigma2", "sigma2", value = .325, min = 0)))
+            
+        } else if(input$prior == "Skeptical"){
+            updateNumericInput(session, "n", value = 2)
+            output$lambda <- renderUI(list(numericInput("p1", "p1", value = .4, min = 0, max = 1),
+                                           numericInput("p2", "p2", value = .6, min = 0, max = 1)))
+            
+            output$mu     <- renderUI(list(numericInput("mu1", "mu1", value = 0),
+                                           numericInput("mu2", "mu2", value = -.38)))
+            
+            output$sigma  <- renderUI(list(numericInput("sigma1", "sigma1", value = 0.325, min=0), 
+                                           numericInput("sigma2", "sigma2", value = 0.325, min=0)))
+        } else if(input$prior == "Cautiously Optimistic"){
+            updateNumericInput(session, "n", value = 1)
+            output$lambda <- renderUI(list(numericInput("p1", "p1", value = 1, min = 0, max = 1)))
+            output$mu     <- renderUI(list(numericInput("mu1", "mu1", value = -.5)))
+            output$sigma  <- renderUI(list(numericInput("sigma1", "sigma1", value = 0.325, min=0)))
+            
+        } else if(input$prior == "Optimistic"){
+            updateNumericInput(session, "n", value = 1)
+            output$lambda <- renderUI(list(numericInput("p1", "p1", value = 1, min = 0, max = 1)))
+            output$mu     <- renderUI(list(numericInput("mu1", "mu1", value = -.7)))
+            output$sigma  <- renderUI(list(numericInput("sigma1", "sigma1", value = 0.1, min=0)))
+        } else if(input$prior == "Very Optimistic"){
+            updateNumericInput(session, "n", value = 1)
+            output$lambda <- renderUI(list(numericInput("p1", "p1", value = 1, min = 0, max = 1)))
+            output$mu     <- renderUI(list(numericInput("mu1", "mu1", value = -1.2)))
+            output$sigma  <- renderUI(list(numericInput("sigma1", "sigma1", value = 0.1, min=0)))
+        } else if(input$prior == "Custom"){
+            enable("n")
+            output$lambda <- renderUI({
+                map(lambda_names(), ~ numericInput(.x, .x, value = isolate(input[[.x]]), min=0, max=1 ) %||% 0)
+            })
+            
+            output$mu <- renderUI({
+                map(mu_names(), ~ numericInput(.x, .x, value = isolate(input[[.x]])) %||% 0)
+            })
+            
+            output$sigma <- renderUI({
+                map(sigma_names(), ~ numericInput(.x, .x, value = isolate(input[[.x]]), min = 0) %||% 0)
+            })
+        } 
+    }, ignoreInit = FALSE)
     
 # Reset button ------------------------------------------------------------
 
     observeEvent(input$reset, {
-        reset("N")
-        reset("rratio")
-        reset("cases")
-        reset("vax_cases")
+        reset("protocol")
+        reset("prior")
         reset("alpha")
         reset("beta")
         reset("n")
@@ -94,37 +168,54 @@ shinyServer(function(input, output) {
 
 # Plot of theta0 ----------------------------------------------------------
 
-    output$theta_prior <- renderPlot({
-        ggplot(data.frame(x = c(0,1)), ggplot2::aes(x)) +
-            stat_dist("prior", size = 1, fun = dbeta,
-                      args = list(shape1 = input$alpha, shape2 = input$beta)) + 
-            geom_line(data=data.frame(x = dens()$dens_theta0$x, y = dens()$dens_theta0$y), aes(x,y, col="posterior"),size=1) + 
+    output$theta_prior <- renderPlotly({
+        x <- seq(0,1,.01)
+        y <- dbeta(x, input$alpha, input$beta)
+        plot <- ggplot(data.frame(x=x,y=y), aes(x,y)) + 
+            geom_line(aes(col="prior")) +
+            #geom_line(data=data.frame(x = dens()$dens_theta0$x, y = dens()$dens_theta0$y), aes(x,y, col="posterior"),size=1) + 
             theme_bw() + 
             geom_hline(yintercept=0) + 
             geom_vline(xintercept=0) + 
             ggtitle("Placebo incidence (theta0)") + 
             ylab("Density") + 
-            xlab("theta0") + 
-            xlim(input$theta_x)
+            xlab("theta0")
+            
+        ggplotly(plot)
     
-    }, res = 96)
+    })
 
 # Plot of VE --------------------------------------------------------------
 
-    output$ve_prior <- renderPlot({
+    output$ve_prior <- renderPlotly({
         lambda <- map_dbl(lambda_names(), ~ default_val(input[[.x]], NA))
-        mu <- map_dbl(mu_names(), ~ default_val(input[[.x]], NA))
-        sigma <- map_dbl(sigma_names(), ~ default_val(input[[.x]], NA))
-        ggplot(data.frame(x = c(-.5,.5)), ggplot2::aes(x)) + 
-            stat_dist("prior", size = 1, fun = dmix,
-                      args = list(p=lambda, mu = mu, sigma = sigma)) + 
-            geom_line(data=data.frame(x = dens()$dens_ve$x, y = dens()$dens_ve$y), aes(x,y, col="posterior"), size=1) + 
-            theme_bw() + 
-            geom_vline(xintercept=0.5, linetype= "dashed") + 
-            geom_hline(yintercept=0) + 
-            ggtitle("Vaccine Efficacy (1-RR)") + 
-            ylab("Density") + 
+        mu     <- map_dbl(mu_names(), ~ default_val(input[[.x]], NA))
+        sigma  <- map_dbl(sigma_names(), ~ default_val(input[[.x]], NA))
+    
+        x <- seq(-1,1,.01)
+        y <- dmix(x, p=lambda, mu=mu, sigma=sigma)
+        
+        plot <- ggplot(data.frame(x=x,y=y), aes(x,y)) +
+            geom_line(aes(col="prior")) + 
+            #geom_line(data=data.frame(x = dens()$dens_ve$x, y = dens()$dens_ve$y), aes(x,y, col="posterior"), size=1) +
+            theme_bw() +
+            geom_vline(xintercept=0.5, linetype= "dashed") +
+            geom_hline(yintercept=0) +
+            ggtitle("Vaccine Efficacy (1-RR)") +
+            ylab("Density") +
             xlab("VE") + 
             xlim(input$ve_x)
-    }, res = 96)
+        
+        
+        if(input$do_sim==0){
+            ggplotly(plot)
+        } else{
+            ggplotly(plot +
+                geom_line(data=data.frame(x = dens()$dens_ve$x, 
+                                          y = dens()$dens_ve$y), 
+                          aes(x,y, col="posterior"), size=1)) 
+                
+        }
+        
+    })
 })
--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinyjs)
+library(plotly)
 
 shinyUI(fluidPage(
       # Need this for reset to work
@@ -25,6 +26,17 @@ shinyUI(fluidPage(
 
           wellPanel(
             fluidRow(
+              column(4,
+                     selectInput("protocol", "Select interim analysis protocol", 
+                                 c("Moderna 1", "Moderna 2", "Pfizer 1", "Custom"))
+              ),
+              column(6, 
+                     selectInput("prior", "Select prior on Vaccine Efficacy", 
+                                 c("Very Skeptical", "Skeptical", "Cautiously Optimistic", "Optimistic", "Very Optimistic", "Custom"))
+              )
+            ),
+            
+            fluidRow(
 
 # First column of data parameters -----------------------------------------
 
@@ -36,20 +48,12 @@ shinyUI(fluidPage(
                      numericInput("vax_cases", "Vaccine cases", value = 10, min = 0, step = 1)
               ),
 
-# Second column of Beta priors --------------------------------------------
-
-              column(2,
-                     h4("Beta prior"),
-                     numericInput("alpha", "Alpha parameter", value = .15, min = 0),
-                     numericInput("beta", "Beta parameter", value = 10, min = 0)
-              ),
-
-# Third column of Gaussian mixture priors ---------------------------------
+# Second column of Gaussian mixture priors ---------------------------------
 
               column(6,
                      h4("Gaussian mixture prior on log(RR)"),
                      numericInput('n', 'Number of mixture components', 
-                                  value = 0, 
+                                  value = 2, 
                                   min = 0, 
                                   step = 1),
                      fluidRow(
@@ -57,19 +61,31 @@ shinyUI(fluidPage(
                        column(3, uiOutput("mu")),
                        column(3, uiOutput("sigma"))
                      )
-              )
+              ),
+
+# Third column of Beta priors --------------------------------------------
+
+              column(2,
+                     h4("Beta prior"),
+                     numericInput("alpha", "Alpha parameter", value = 4, min = 0),
+                     numericInput("beta", "Beta parameter", value = 160, min = 0)
+              ),
             )
           )
         ),
 
 # Plotting Side -----------------------------------------------------------
-
+      
         column(6,
-          titlePanel("Prior and Posterior Graph"),
-          plotOutput("theta_prior"),
-          sliderInput("theta_x", "x-axis", min=0, max=1, step=0.01, value=c(0,1)),
-          plotOutput("ve_prior"),
-          sliderInput("ve_x", "x-axis", min=-2, max=2, step=0.1, value=c(-.5,.5))
+               tabsetPanel(type = "tabs",
+                           tabPanel("Plot",
+                                    plotlyOutput("ve_prior"),
+                                    sliderInput("ve_x", "VE x-axis", min=-2, max = 2, value = c(-.5,.5), step = .01),
+                                    plotlyOutput("theta_prior")
+                           ),
+                           tabPanel("Documentation"),
+                           tabPanel("Console Output")
+               )
         )
       )
 ))
